99f163c6f8e555a883e675c4366f87df9a6fadd4
Robert Gant <demontegant@gmail.com>
2019-08-02 15:12:31 -0500

    pushing through all code

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,65 @@
-const express = 'express';
+const express = require('express');
+const Posts = require('./postDb');
+const User = require('../users/userDb');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Posts.get(req.query);
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the posts'
+    });
+  }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validatePostId, async (req, res) => {
+  res.status(200).json(req.post);
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validatePostId, async (req, res) => {
+  try {
+    const count = await Posts.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: 'Post has been deleted!' });
+    } else {
+      res.status(404).json({ message: 'Aww man we could not find the id' });
+    }
+  } catch {}
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validatePostId, async (req, res) => {
+  try {
+    const post = await Posts.update(req.params.id, req.body);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Sorry the post cannot be found!' });
+    }
+  } catch (err) {
+    res.status(500).json(error);
+  }
 });
 
 // custom middleware
+async function validatePostId(req, res, next) {
+  try {
+    const { id } = req.params;
 
-function validatePostId(req, res, next) {
+    const post = await Posts.getById(id);
 
-};
+    if (post) {
+      req.post = post;
+      next();
+    } else {
+      res.status(404).json({ message: 'ID is not found bud!' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,126 @@
-const express = 'express';
+const express = require('express');
+const Posts = require('../posts/postDb');
+const db = require('./userDb');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+// router.post('/', async (req, res) => {
+//   try {
+//     const user = await Users.add(req.body);
+//     res.status(200).json(user);
+//   } catch (error) {
+//     res.status();
+//   }
+// });
 
-});
+// router.post('/:id/posts', (req, res) => {});
 
-router.post('/:id/posts', (req, res) => {
+// router.get('/', (req, res) => {});
 
-});
+// router.get('/:id', (req, res) => {});
 
-router.get('/', (req, res) => {
+// router.get('/:id/posts', (req, res) => {});
 
-});
+// router.delete('/:id', (req, res) => {});
 
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+// router.put('/:id', (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+router.get('/', async (req, res) => {
+  try {
+    const users = await db.get();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving users' });
+  }
+});
 
-};
+router.post('/', async (req, res) => {
+  try {
+    if (req.body.name) {
+      const newPost = await db.insert(req.body);
+      res.status(201).json(newPost);
+    } else {
+      res.status(400).json({ message: 'Please provide a name.' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error posting to database' });
+  }
+});
 
-function validateUser(req, res, next) {
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const remove = await db.remove(id);
+    remove
+      ? res.status(200).end()
+      : res.status(404).json({ message: 'No user by that id' });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: 'Everything exploded, sorry.  Try again.' });
+  }
+});
 
-};
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (req.body.name) {
+      const updateUser = await db.update(id, req.body);
+      updateUser
+        ? res.status(200).end()
+        : res.status(404).json({ message: 'No user by that ID found' });
+    } else {
+      res.status(400).json({ message: 'I need the new name you use to use' });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'There was an error updating the user' });
+  }
+});
 
-function validatePost(req, res, next) {
+router.get('/:id/posts', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userPosts = await db.getUserPosts(id);
+    res.status(200).json({ userPosts });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Something went wrong getting the user's posts" });
+  }
+});
 
-};
+// async function validatePostId(req, res, next) {
+//   try {
+//     const { id } = req.params;
 
+//     const post = await Posts.getById(id);
+
+//     if (post) {
+//       req.post = post;
+//       next();
+//     } else {
+//       res.status(404).json({ message: 'ID is not found bud!' });
+//     }
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// }
+
+// function requireBody(req, res, next) {
+//   if (req.body && Object.keys(req.body).length > 0) {
+//     next();
+//   } else {
+//     next({ message: 'You need to supply a body with this request' });
+//   }
+// }
+
+// function validateUser(req, res, next) {}
+
+// function validatePost(req, res, next) {}
 module.exports = router;
